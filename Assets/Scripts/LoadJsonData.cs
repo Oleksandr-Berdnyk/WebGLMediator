7afc029bdc173752eb0b7c0077b0489bf0d55784
Oleksandr-Berdnyk <oleksandr.ber@logicoapps.com>
2024-08-09 15:50:02 +0300

    create project

---
+++ Assets/Scripts/LoadJsonData.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class LoadJsonData : MonoBehaviour, ILoadData
+    {
+        [System.Serializable]
+        public class URIData
+        {
+            public string URL; 
+        }
+
+        public string GetURI()
+        {
+            string url = string.Empty;
+            TextAsset jsonFile = Resources.Load<TextAsset>("GameUrl");
+
+            if (jsonFile != null)
+            {
+                URIData data = JsonUtility.FromJson<URIData>(jsonFile.text);
+
+                Debug.Log("Loaded URL: " + data.URL);
+                url = data.URL;
+            }
+            else
+            {
+                Debug.LogError("File not found in Resources folder");
+            }
+
+            return url;
+        }
+    }
+}
